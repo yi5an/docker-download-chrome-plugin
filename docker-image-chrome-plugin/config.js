c94f48c9e769d051619b9ce510e9247170a67655
dgyuan <1149508162@qq.com>
2026-02-01 18:07:35 +0800

    fix: 更新代理配置，修复追踪URL和默认代理地址

--- docker-image-chrome-plugin/config.js
+++ docker-image-chrome-plugin/config.js
@@ -1,6 +1,6 @@
 // config.js
 // 统一维护中转服务器地址
-// const DEFAULT_PROXY_BASE = 'http://123.57.165.38:7000/proxy?url=';
-// const TRACKING_URL = 'http://123.57.165.38:3000/api/track'; // 修改为实际地址或本地调试地址
-const DEFAULT_PROXY_BASE = 'http://192.168.0.102:7000/proxy?url=';
-const TRACKING_URL = 'http://192.168.0.102:3000/api/track'; // 修改为实际地址或本地调试地址
+const DEFAULT_PROXY_BASE = 'http://123.57.165.38:7000/proxy?url=';
+const TRACKING_URL = 'http://123.57.165.38:3000/api/track'; // 修改为实际地址或本地调试地址
+// const DEFAULT_PROXY_BASE = 'http://192.168.0.102:7000/proxy?url=';
+// const TRACKING_URL = 'http://192.168.0.102:3000/api/track'; // 修改为实际地址或本地调试地址
